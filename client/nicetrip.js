8750f0a3cbc56056477f12d29b34c9b330f438f4
Goundo Djikine <goundo-djikine@cifacom.com>
2015-05-11 21:49:23 +0200

    first commit

---
+++ client/nicetrip.js
@@ -0,0 +1,47 @@
+Trips = new Mongo.Collection("trips");
+
+
+Template.trips.helpers({
+    'Trips' : Trips.find({}, {sort : {dest : 1}})
+});
+
+
+// Helpers
+/*Template.nicetrip.helpers
+(
+  {
+
+    trips : function () {
+            console.log(Trips);
+            return Trips.find().fetch(
+
+                function( trip, index, cursor ){
+
+                    return { _id : trip._id, dest: trip.dest, arrival : trip.arrival };
+            }
+
+          );
+    }
+  }
+);
+*/
+// Events
+Template.nicetrip.events = {
+
+
+    'submit .new-trip' : function ( e, template )
+    {
+            e.preventDefault();
+
+            var $dest = template.find( "#dest" );
+            var $arrival = template.find( "#arrival" );
+
+      if ( $dest.value !== "" && $arrival.value !== "" )
+      {
+       Trips.insert( {dest : $dest.value, arrival : $arrival.value} );
+      }
+    }
+
+};
+
+
